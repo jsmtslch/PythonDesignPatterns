7eefc12eaca66fd8c174e5ebd1f6800cbc29c3aa
Jasmeet Singh <jasmeet.singh@staples.com>
2019-12-12 12:38:57 -0500

    updated
    Co-Authored-By: JSMTSLCH <jsmtslch@users.noreply.github.com>

--- Strategy/Strategy.py
+++ Strategy/Strategy.py
@@ -1,3 +1,41 @@
 from StrategyClass import LoudQuackStrategy
 from StrategyClass import GentleQuackStrategy
 from StrategyClass import LightOnStrategy
+
+loud_quack = LoudQuackStrategy()
+gentle_quack = GentleQuackStrategy()
+ten_seconds = LightOnStrategy()
+
+class Duck(object):
+	def __init__(self, quack_strategy, light_strategy):
+		self._quack_strategy = quack_strategy
+		self._light_strategy = light_strategy
+	def quack(self):
+		self._quack_strategy.quack()
+	def light_on(self):
+		self._light_strategy.light_on()
+
+class VillageDuck(Duck):
+	def __init__(self):
+		super(VillageDuck, self).__init__(loud_quack, None)
+	def go_home(self):
+		print("Going to Village")
+		
+class ToyDuck(Duck):
+	def __init__(self):
+		super(ToyDuck, self).__init__(gentle_quack, ten_seconds)
+
+class CityDuck(Duck):
+	def __init__(self):
+		super(CityDuck, self).__init__(gentle_quack, None)
+	
+	def go_home(self):
+		print("Going to City")
+
+class RoboDuck(Duck):
+	def __init__(self):
+		super(RoboDuck, self).__init__(loud_quack, ten_seconds)
+
+robo = RoboDuck()
+robo.quack()
+robo.light_on()
